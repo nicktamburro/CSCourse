6731a8215fbe8340c86b8a6412a10ddb751ede1e
nicktamburro <nicholastamburro@gmail.com>
2018-05-05 21:15:52 -0700

    fixed dumb mistakes

--- NewInsertion.js
+++ NewInsertion.js
@@ -1,10 +1,10 @@
 function Insertion(array){
 
 for(let i = 1; i<array.length; i++){
-	for(let j=0; j<i; i++){
+	for(let j=0; j<i; j++){
 		if(array[i]<array[j]){
 			let spliced = array.splice(i, 1);
-			array.splice(j, 1, spliced[0]);
+			array.splice(j, 0, spliced[0]);
 		}
 	}
 	
