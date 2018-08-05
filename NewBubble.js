8928751acaebe3aa0b30e95b39ca35994aab184a
nicktamburro <nicholastamburro@gmail.com>
2018-08-04 21:37:25 -0700

    talking myself through algorithms again

--- NewBubble.js
+++ NewBubble.js
@@ -7,13 +7,17 @@ let swap = true;
 do{
 	//loop through all the numbers, if you find any out of order, swap them
 	for (i = 0; i<array.length; i++){
+		//if the first number is bigger than the second... 
 		if(array[i] > array[i+1]){
+			//use this three-step swapping process, t = temporary
 			let t = array[i];
 			array[i] = array[i+1];
 			array[i+1] = t;
 			//and swap is still true, since we made a swap
+			//set swap to true, still
 			swap = true;
 		}else{
+			//no swamps, swap = false
 			//if no swaps, swap = false
 			swap = false;
 		}
